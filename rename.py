114177e09d55ed205cbeaf2635087a4c0f1619a9
hekte <85772458+hekte@users.noreply.github.com>
2023-04-07 10:51:30 +1000

    Add source directory argument
    Prompt: Can you make the directory a command line option?
    ChatGPT: Yes, I can modify the script to allow you to specify the directory as a command line argument. Here's the updated script:

--- rename.py
+++ rename.py
@@ -1,16 +1,19 @@
 import os
 import datetime
 from PIL import Image
+import argparse
 
-# Set the directory where your photos are located
-directory = "/path/to/photos"
+# Set up command line argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument("directory", help="the directory containing the photos")
+args = parser.parse_args()
 
 # Loop through all files in the directory
-for filename in os.listdir(directory):
+for filename in os.listdir(args.directory):
     if filename.endswith(".jpg"):
         
         # Open the image and get the original date and time from the EXIF metadata
-        with Image.open(os.path.join(directory, filename)) as image:
+        with Image.open(os.path.join(args.directory, filename)) as image:
             exif_data = image._getexif()
             if exif_data is not None and 36867 in exif_data:
                 date_time_str = exif_data[36867]
@@ -18,7 +21,7 @@ for filename in os.listdir(directory):
                 date = date_time_obj.strftime('%Y-%m-%d_%H%M%S')
             else:
                 # If no EXIF data is available, use the file's creation time instead
-                timestamp = os.path.getctime(os.path.join(directory, filename))
+                timestamp = os.path.getctime(os.path.join(args.directory, filename))
                 date = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d_%H%M%S')
         
         # Construct the new file name
@@ -26,9 +29,9 @@ for filename in os.listdir(directory):
         
         # Handle conflicts by appending a suffix
         counter = 1
-        while os.path.exists(os.path.join(directory, new_filename)):
+        while os.path.exists(os.path.join(args.directory, new_filename)):
             new_filename = f"{date}_{counter}.jpg"
             counter += 1
         
         # Rename the file
-        os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+        os.rename(os.path.join(args.directory, filename), os.path.join(args.directory, new_filename))
