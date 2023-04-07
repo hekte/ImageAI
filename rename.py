d05ad1a76e9e3a9bcdb2ed5fe3d745da4957b4f9
hekte <85772458+hekte@users.noreply.github.com>
2023-04-07 10:50:05 +1000

    Add pillow to extract EXIF
    Prompt: Can you extract the original date and time from the image and use that as the name?
    
    ChatGPT: Yes, you can extract the original date and time from the image's metadata and use that as the name. Here's an updated script that uses the Pillow library to read the EXIF data from each image and extract the original date and time:

--- rename.py
+++ rename.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+from PIL import Image
 
 # Set the directory where your photos are located
 directory = "/path/to/photos"
@@ -8,17 +9,25 @@ directory = "/path/to/photos"
 for filename in os.listdir(directory):
     if filename.endswith(".jpg"):
         
-        # Get the creation time of the file
-        timestamp = os.path.getctime(os.path.join(directory, filename))
-        date = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
+        # Open the image and get the original date and time from the EXIF metadata
+        with Image.open(os.path.join(directory, filename)) as image:
+            exif_data = image._getexif()
+            if exif_data is not None and 36867 in exif_data:
+                date_time_str = exif_data[36867]
+                date_time_obj = datetime.datetime.strptime(date_time_str, '%Y:%m:%d %H:%M:%S')
+                date = date_time_obj.strftime('%Y-%m-%d_%H%M%S')
+            else:
+                # If no EXIF data is available, use the file's creation time instead
+                timestamp = os.path.getctime(os.path.join(directory, filename))
+                date = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d_%H%M%S')
         
         # Construct the new file name
-        new_filename = f"{date}_{filename}"
+        new_filename = f"{date}.jpg"
         
         # Handle conflicts by appending a suffix
         counter = 1
         while os.path.exists(os.path.join(directory, new_filename)):
-            new_filename = f"{date}_{counter}_{filename}"
+            new_filename = f"{date}_{counter}.jpg"
             counter += 1
         
         # Rename the file
